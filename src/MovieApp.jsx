64398455320e5eda8748c59c05347a33d0933963
@diegote22 <diegote_21_82@hotmail.com>
2024-06-07 21:26:59 -0300

    Aplicación de búsqueda de películas

---
+++ src/MovieApp.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './MovieApp.css'
+
+export const MovieApp = () => {
+
+    const [search, setSearch] = useState('')
+    const [movieList, setMovieList] = useState(null)
+
+    const urlBase = 'https://api.themoviedb.org/3/search/movie'
+    const API_KEY = 'TU_API_KEY'
+
+    const handleInputChange = ({ target }) => {
+        setSearch(target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        fetchMovies()
+    }
+
+    const fetchMovies = async () => {
+        try {
+            const response = await fetch(`${urlBase}?query=${search}&api_key=${API_KEY}&language=es-ES`)
+            const data = await response.json()
+            setMovieList(data.results)
+        } catch (error) {
+            console.error('Ha ocurrido el siguiente error: ', error)
+        }
+    }
+
+    return (
+        <div className='container'>
+            <h1>Buscador de Películas</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='Escribí una película'
+                    value={search}
+                    onChange={handleInputChange}
+                />
+                <button>Buscar</button>
+            </form>
+            {movieList &&
+                <div className='movie-list'>
+                    {movieList.map(movie => (
+                        <div key={movie.id} className='movie-card'>
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                            <h2>{movie.title}</h2>
+                            <p>{movie.overview}</p>
+                        </div>
+                    ))}
+                </div>
+            }
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { MovieApp } from './MovieApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MovieApp/>
+  </React.StrictMode>,
+)
